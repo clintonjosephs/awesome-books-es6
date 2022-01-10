61e491a9989764017beb7d71bf4d2ab20993c156
clintonjosephs <clintonjosephs26@gmail.com>
2022-01-10 17:11:04 +0100

    Manage javascript linters errors

--- modules/storage.js
+++ modules/storage.js
@@ -2,6 +2,7 @@ export default class storageManager {
   static storeData(booksData) {
     localStorage.setItem('bookData', JSON.stringify(booksData));
   }
+
   static getData() {
     return JSON.parse(localStorage.getItem('bookData'));
   }
