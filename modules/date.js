801f32d014563071a566d9bd4e9f49ecbc3ba8f9
clintonjosephs <clintonjosephs26@gmail.com>
2022-02-13 10:33:17 +0100

    added timezone to datetime display

--- modules/date.js
+++ modules/date.js
@@ -2,5 +2,5 @@ import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
 
 export default function luxon() {
   const dt = DateTime.now();
-  return dt.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+  return dt.toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
 }
